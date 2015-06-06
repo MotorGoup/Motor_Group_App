0d5dfa9ce12f0553572a8223a24869f9e1e61742
karim herrera <karim@MacBook-Air-de-karim.local>
2015-06-06 18:18:35 -0500

    Initial Commit

---
+++ autos/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  autos
+//
+//  Created by karim herrera on 06/06/15.
+//  Copyright (c) 2015 In-die Apps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
